eeeef4223e9ca741ad821f24799540e522e704e7
Nikunj Chauhan <nkca122@gmail.com>
2026-05-23 15:11:37 +0530

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.py
@@ -0,0 +1,26 @@
+def str_sum(num1 : str, num2 : str) -> str:
+    return f"{int(num1) + int(num2)}"
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        ptr = len(num2) - 1; global product; global carry; carry = 0; global it; res = "0"
+        while ptr >= 0:
+            num2_digit = int(num2[ptr]); it = "0" * (len(num2) - ptr - 1)
+            for num1_digit in num1[::-1]:
+                product = (int(num1_digit) * int(num2_digit)) + carry
+                carry = product // 10
+                product %= 10
+                it = f"{product}{it}"
+            ptr -= 1
+            if carry:
+                it = f"{carry}{it}"
+            carry = 0
+            print(it)
+            res = str_sum(res, it)
+        return res
+        
+
+
+
+                
+
+        
